dce62a3731262aafba2340d836f5e2369b2c3dd1
Stacy Jones <stacyjones12335@gmail.com>
2024-03-10 19:24:28 -0400

    fixed the BMI calculator

--- BMI calculator.py
+++ BMI calculator.py
@@ -1,10 +1,10 @@
-weight = input(float("Enter your weight in pounds: "))
-height = input(float("Enter your weight in inches: "))
+weight = input("Enter your weight in pounds: ")
+height = input("Enter your height in inches: ")
 
-weight_in_kgs = weight * 0.45359237
+weight_in_kgs = float(weight) * 0.45359237
 
-height_in_m = height * 0.0254
+height_in_m = float(height) * 0.0254
 
 BMI = weight_in_kgs / (height_in_m ** 2)
 
-print("Your BMI is " + str(BMI))
+print("Your BMI is ", BMI)
